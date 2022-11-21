2d7a05be51dd585bd1d743db396b6ab1a285a551
Cstrp <valery.nosareu@gmail.com>
2022-11-21 15:20:39 +0300

    create project | folders

--- Client/src/app/app.component.spec.ts
+++ Client/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'NotesApp'`, () => {
+  it(`should have as title 'NoteApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('NotesApp');
+    expect(app.title).toEqual('NoteApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('NotesApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('NoteApp app is running!');
   });
 });
